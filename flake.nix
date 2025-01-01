76cab75f853429717dd49dd1dc09561d4bfeeaba
eprader <56026248+eprader@users.noreply.github.com>
2025-01-01 18:28:31 +0100

    nix(flake): initial `nix` setup

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Ansible playbook to set up a k3s cluster.";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+  };
+
+  outputs = { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+
+    in
+    {
+      devShells.${system} = {
+        default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            ansible
+            kubectl
+          ];
+
+          shellHook = ''
+            export PS1="(nix-shell) $PS1" # NOTE: To communicate that a nix shell is active
+          '';
+        };
+      };
+    };
+}
